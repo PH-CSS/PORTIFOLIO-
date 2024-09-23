4c20c2abb7e6c8a1244328292fe0fbd44a9d9742
Paulinho <paulohpeixinhoenrique@gmail.com>
2024-09-22 21:53:30 -0300

    progresso de modelo questions

--- Portifolio/Escola-segundo-ano/IW2-teste/QUIZcookie/sourse/index.js
+++ Portifolio/Escola-segundo-ano/IW2-teste/QUIZcookie/sourse/index.js
@@ -1,24 +1,85 @@
-const changeMode = document.querySelectorAll(".change");
-const root = document.documentElement;
+const colorTheme = document.querySelectorAll(".change");
+const subTitle = document.querySelector(".sub")
+const titleName = document.querySelector(".nameText span");
+const bgImage = document.querySelector(".round img")
+const secondarybgImage = document.querySelector(".otherMode img")
+// console.dir(secondarybgImage.attributes[0].nodeValue);
 
+const changeModeByIMG = document.querySelector(".otherMode")
+let NodeValNomeBG = changeModeByIMG.attributes.name.nodeValue;
+changeModeByIMG.addEventListener("click", () =>{
+  changeMode(changeModeByIMG, NodeValNomeBG) 
+  NodeValNomeBG = (NodeValNomeBG == "sun")? "moon": "sun" ;
+})
+
+
+console.log(titleName);
+
+const root = document.documentElement;
 // console.log(getComputedStyle(root).getPropertyValue("--bg-color"))
-changeMode.forEach(mode => {
-  mode.addEventListener("click", (e) => {
-    console.log(mode.name);
+// sintaxe para a mudança de cor
+
+colorTheme.forEach(mode => {
+  mode.addEventListener("click", () => changeMode(mode, mode.name));
+});
+
+function changeMode(mode, NameNode) {
+    // console.dir(secondarybgImage.attributes.name.nodeValue);
+
+    // paleta de cores da mudança de quiz
     const dayNight = {
-      day:"rgb(0, 143, 143)",
-      night:"rgb(2, 68, 68)"
+      sun:[
+      "#0288D1",
+       "background-image: url(../img/sun-svgrepo-white-com.svg);"
+      ],
+      moon:[
+        "rgb(2, 68, 68)",
+        "background-image: url(../img/moon-svgrepo-com.svg);"
+      ]
     };
     const bgMode = {
-      day:"rgb(157, 253, 253)",
-      night:"rgb(134, 185, 185)"
+      sun:"#B3E5FC",
+      moon:"rgb(134, 185, 185)"
     }
-    let nomeDoEscolhido = mode.name;
-    root.style.setProperty('--bg-color', dayNight[nomeDoEscolhido])
+
+    let nomeDoEscolhido = NameNode;
+    // propriedade para mudança de cor
+    root.style.setProperty('--bg-color', dayNight[nomeDoEscolhido][0])
     root.style.setProperty('--bg-body', bgMode[nomeDoEscolhido])
 
-})
-});
+    // identificação para qual palavra colocar no titulo
+    subTitle.textContent = `The ${nomeDoEscolhido}`;
+    let artigo = (nomeDoEscolhido == "moon")? "a lua": "o sol";
+
+    if (titleName.textContent !== artigo) {
+      
+      titleName.classList.toggle('sairEntrar');
+      secondarybgImage.classList.toggle('sairEntrar');
+      bgImage.classList.toggle('sairEntrar');
+
+
+      setTimeout(() => {
+        titleName.textContent = `${artigo}`
+        // animação de mudança para titulo principal
+        if (nomeDoEscolhido == "moon") {
+          secondarybgImage.attributes[0].nodeValue = "./img/sun-svgrepo-com.svg"
+          bgImage.attributes[0].nodeValue = "./img/moon-white-svgrepo-com.svg"
+        } else{
+          bgImage.attributes[0].nodeValue = "./img/sun-svgrepo-white-com.svg"
+          secondarybgImage.attributes[0].nodeValue = "./img/moon-svgrepo-com.svg"
+        }
+        
+      }, 500);
+
+      setTimeout(() => {
+        titleName.classList.toggle('sairEntrar');
+        secondarybgImage.classList.toggle('sairEntrar');
+        bgImage.classList.toggle('sairEntrar');
+      }, 1000);
+    }
+
+
+}
 
 
 
--- Portifolio/treinosDeElementos/newLanguage/DOTNET/obj/Debug/net8.0/Portifolio.AssemblyInfo.cs
+++ Portifolio/treinosDeElementos/newLanguage/DOTNET/obj/Debug/net8.0/Portifolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portifolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e71c47808c69996a44c3c182d93ecf1b769b43a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35f4747e0a93232564c200169ea60ef2227da459")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portifolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portifolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
