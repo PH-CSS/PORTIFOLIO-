eb323cee8d463c88d7c8c4766baf3e7e7c6a5da7
Paulinho <paulohpeixinhoenrique@gmail.com>
2025-02-14 20:14:54 -0300

    Para atualizar o processo

---
+++ Portifolio/extraCurriculares/Spotify/Aula-1/Gabarito-da-Aula-1/src/App.jsx
@@ -0,0 +1,8 @@
+import Header from './components/header'
+import './index.css'
+// rafce
+const App = () => {
+  return <Header />
+}
+
+export default App
---
+++ Portifolio/extraCurriculares/Spotify/Aula-1/Gabarito-da-Aula-1/src/components/header.jsx
@@ -0,0 +1,11 @@
+import logoSpotify from '../assets/logo/spotify-logo.png'
+
+const Header = () => {
+  return (
+    <div>
+      <img src={logoSpotify} alt="" />
+    </div>
+  )
+}
+
+export default Header
